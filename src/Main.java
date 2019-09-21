28058ff6a385021d1e436a7a77945f83a089f988
hilty8 <biblioteca.financial.nine@gmail.com>
2019-09-21 18:57:20 +0900

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	public static void main(String[] args) {
+		// BookShelf の大きさを指定
+		BookShelf bookShelf = new BookShelf(4);
+		bookShelf.appendBook(new Book("「人間嫌い」のルール"));
+		bookShelf.appendBook(new Book("孤独について"));
+		bookShelf.appendBook(new Book("人生を〈半分〉降りる"));
+		bookShelf.appendBook(new Book("今日の芸術"));
+		Iterator it = bookShelf.iterator();
+		while(it.hasNext()) {
+			Book book = (Book)it.next();
+			System.out.println(book.getName());
+		}
+	}
+}
